ca83331e1485ba7e4b2c3fd09a8f3e194bc85281
808257 <parassingh.singh15@gmail.com>
2022-07-21 13:42:34 +0530

    playlist modal added

--- Muzik-App-/Client/src/components/SliderComponent_4/SliderComponent_4.js
+++ Muzik-App-/Client/src/components/SliderComponent_4/SliderComponent_4.js
@@ -1,21 +1,42 @@
 import React from "react";
 import "./SliderComponent_4.css";
 import Card from "@mui/material/Card";
+
+import { useState } from "react";
+import { Modal } from "@mantine/core";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { CardActionArea } from "@mui/material";
 import Login from "../../img/login.PNG";
+import AddToPlaylistModal from "../AddToPlaylistModal/AddToPlaylistModal";
 
 export default function SliderComponent_4() {
+  const [opened, setOpened] = useState(false);
+
   return (
     <div className="SliderComponent_4">
+      <Modal
+        className="CreatePlaylistDiv"
+        opened={opened}
+        onClose={() => setOpened(false)}
+        title="Add To Playlist"
+      >
+        <AddToPlaylistModal />
+      </Modal>
       <h2 className="SliderComponentHeading SongComponentHeading">
         Weekly Charts
       </h2>
       <div className="Sub-SliderComponent">
         <div className="SliderComponent_2_card">
-          <Card className="cards-layout">
+          <Card
+            className="cards-layout"
+            sx={{
+              backgroundColor: "#485461",
+              backgroundImage:
+                "linear-gradient(315deg, #485461 0%, #28313b 74%)",
+            }}
+          >
             <CardActionArea>
               <CardMedia
                 component="img"
@@ -23,25 +44,38 @@ export default function SliderComponent_4() {
                 image={Login}
                 alt="green iguana"
               />
-              <CardContent className='cardComponent'>
-          <div className="card-heading">
-          <Typography gutterBottom variant="h5" component="div">
-            Song Name
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Artist Name
-          </Typography>
-          </div>
-          <div className="playBtn">
-          <i class="ss fa-solid fa-circle-play"></i>
-          </div>
-        </CardContent>
+              <CardContent className="cardComponent">
+                <div className="card-heading">
+                  <Typography gutterBottom variant="h5" component="div">
+                    Song Name
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Artist Name
+                  </Typography>
+                </div>
+                <div className="playBtn">
+                  <i class="ss fa-solid fa-circle-play"></i>
+                </div>
+              </CardContent>
             </CardActionArea>
+            <i
+              class="slider-component2_heart fa-solid fa-plus float-end text-end"
+              onClick={() => setOpened(true)}
+            ></i>
+
+            <i class="slider-component2_heart fa-regular fa-heart float-end text-end"></i>
           </Card>
         </div>
 
         <div className="SliderComponent_2_card">
-          <Card className="cards-layout">
+          <Card
+            className="cards-layout"
+            sx={{
+              backgroundColor: "#485461",
+              backgroundImage:
+                "linear-gradient(315deg, #485461 0%, #28313b 74%)",
+            }}
+          >
             <CardActionArea>
               <CardMedia
                 component="img"
@@ -49,25 +83,38 @@ export default function SliderComponent_4() {
                 image={Login}
                 alt="green iguana"
               />
-              <CardContent className='cardComponent'>
-          <div className="card-heading">
-          <Typography gutterBottom variant="h5" component="div">
-            Song Name
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Artist Name
-          </Typography>
-          </div>
-          <div className="playBtn">
-          <i class="ss fa-solid fa-circle-play"></i>
-          </div>
-        </CardContent>
+              <CardContent className="cardComponent">
+                <div className="card-heading">
+                  <Typography gutterBottom variant="h5" component="div">
+                    Song Name
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Artist Name
+                  </Typography>
+                </div>
+                <div className="playBtn">
+                  <i class="ss fa-solid fa-circle-play"></i>
+                </div>
+              </CardContent>
             </CardActionArea>
+            <i
+              class="slider-component2_heart fa-solid fa-plus float-end text-end"
+              onClick={() => setOpened(true)}
+            ></i>
+
+            <i class="slider-component2_heart fa-regular fa-heart float-end text-end"></i>
           </Card>
         </div>
 
         <div className="SliderComponent_2_card">
-          <Card className="cards-layout">
+          <Card
+            className="cards-layout"
+            sx={{
+              backgroundColor: "#485461",
+              backgroundImage:
+                "linear-gradient(315deg, #485461 0%, #28313b 74%)",
+            }}
+          >
             <CardActionArea>
               <CardMedia
                 component="img"
@@ -75,25 +122,38 @@ export default function SliderComponent_4() {
                 image={Login}
                 alt="green iguana"
               />
-              <CardContent className='cardComponent'>
-          <div className="card-heading">
-          <Typography gutterBottom variant="h5" component="div">
-            Song Name
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Artist Name
-          </Typography>
-          </div>
-          <div className="playBtn">
-          <i class="ss fa-solid fa-circle-play"></i>
-          </div>
-        </CardContent>
+              <CardContent className="cardComponent">
+                <div className="card-heading">
+                  <Typography gutterBottom variant="h5" component="div">
+                    Song Name
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Artist Name
+                  </Typography>
+                </div>
+                <div className="playBtn">
+                  <i class="ss fa-solid fa-circle-play"></i>
+                </div>
+              </CardContent>
             </CardActionArea>
+            <i
+              class="slider-component2_heart fa-solid fa-plus float-end text-end"
+              onClick={() => setOpened(true)}
+            ></i>
+
+            <i class="slider-component2_heart fa-regular fa-heart float-end text-end"></i>
           </Card>
         </div>
 
         <div className="SliderComponent_2_card">
-          <Card className="cards-layout">
+          <Card
+            className="cards-layout"
+            sx={{
+              backgroundColor: "#485461",
+              backgroundImage:
+                "linear-gradient(315deg, #485461 0%, #28313b 74%)",
+            }}
+          >
             <CardActionArea>
               <CardMedia
                 component="img"
@@ -101,25 +161,38 @@ export default function SliderComponent_4() {
                 image={Login}
                 alt="green iguana"
               />
-              <CardContent className='cardComponent'>
-          <div className="card-heading">
-          <Typography gutterBottom variant="h5" component="div">
-            Song Name
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Artist Name
-          </Typography>
-          </div>
-          <div className="playBtn">
-          <i class="ss fa-solid fa-circle-play"></i>
-          </div>
-        </CardContent>
+              <CardContent className="cardComponent">
+                <div className="card-heading">
+                  <Typography gutterBottom variant="h5" component="div">
+                    Song Name
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Artist Name
+                  </Typography>
+                </div>
+                <div className="playBtn">
+                  <i class="ss fa-solid fa-circle-play"></i>
+                </div>
+              </CardContent>
             </CardActionArea>
+            <i
+              class="slider-component2_heart fa-solid fa-plus float-end text-end"
+              onClick={() => setOpened(true)}
+            ></i>
+
+            <i class="slider-component2_heart fa-regular fa-heart float-end text-end"></i>
           </Card>
         </div>
 
         <div className="SliderComponent_2_card">
-          <Card className="cards-layout">
+          <Card
+            className="cards-layout"
+            sx={{
+              backgroundColor: "#485461",
+              backgroundImage:
+                "linear-gradient(315deg, #485461 0%, #28313b 74%)",
+            }}
+          >
             <CardActionArea>
               <CardMedia
                 component="img"
@@ -127,25 +200,38 @@ export default function SliderComponent_4() {
                 image={Login}
                 alt="green iguana"
               />
-              <CardContent className='cardComponent'>
-          <div className="card-heading">
-          <Typography gutterBottom variant="h5" component="div">
-            Song Name
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Artist Name
-          </Typography>
-          </div>
-          <div className="playBtn">
-          <i class="ss fa-solid fa-circle-play"></i>
-          </div>
-        </CardContent>
+              <CardContent className="cardComponent">
+                <div className="card-heading">
+                  <Typography gutterBottom variant="h5" component="div">
+                    Song Name
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Artist Name
+                  </Typography>
+                </div>
+                <div className="playBtn">
+                  <i class="ss fa-solid fa-circle-play"></i>
+                </div>
+              </CardContent>
             </CardActionArea>
+            <i
+              class="slider-component2_heart fa-solid fa-plus float-end text-end"
+              onClick={() => setOpened(true)}
+            ></i>
+
+            <i class="slider-component2_heart fa-regular fa-heart float-end text-end"></i>
           </Card>
         </div>
 
         <div className="SliderComponent_2_card">
-          <Card className="cards-layout">
+          <Card
+            className="cards-layout"
+            sx={{
+              backgroundColor: "#485461",
+              backgroundImage:
+                "linear-gradient(315deg, #485461 0%, #28313b 74%)",
+            }}
+          >
             <CardActionArea>
               <CardMedia
                 component="img"
@@ -153,25 +239,38 @@ export default function SliderComponent_4() {
                 image={Login}
                 alt="green iguana"
               />
-              <CardContent className='cardComponent'>
-          <div className="card-heading">
-          <Typography gutterBottom variant="h5" component="div">
-            Song Name
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Artist Name
-          </Typography>
-          </div>
-          <div className="playBtn">
-          <i class="ss fa-solid fa-circle-play"></i>
-          </div>
-        </CardContent>
+              <CardContent className="cardComponent">
+                <div className="card-heading">
+                  <Typography gutterBottom variant="h5" component="div">
+                    Song Name
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Artist Name
+                  </Typography>
+                </div>
+                <div className="playBtn">
+                  <i class="ss fa-solid fa-circle-play"></i>
+                </div>
+              </CardContent>
             </CardActionArea>
+            <i
+              class="slider-component2_heart fa-solid fa-plus float-end text-end"
+              onClick={() => setOpened(true)}
+            ></i>
+
+            <i class="slider-component2_heart fa-regular fa-heart float-end text-end"></i>
           </Card>
         </div>
 
         <div className="SliderComponent_2_card">
-          <Card className="cards-layout">
+          <Card
+            className="cards-layout"
+            sx={{
+              backgroundColor: "#485461",
+              backgroundImage:
+                "linear-gradient(315deg, #485461 0%, #28313b 74%)",
+            }}
+          >
             <CardActionArea>
               <CardMedia
                 component="img"
@@ -179,25 +278,36 @@ export default function SliderComponent_4() {
                 image={Login}
                 alt="green iguana"
               />
-              <CardContent className='cardComponent'>
-          <div className="card-heading">
-          <Typography gutterBottom variant="h5" component="div">
-            Song Name
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Artist Name
-          </Typography>
-          </div>
-          <div className="playBtn">
-          <i class="ss fa-solid fa-circle-play"></i>
-          </div>
-        </CardContent>
+              <CardContent className="cardComponent">
+                <div className="card-heading">
+                  <Typography gutterBottom variant="h5" component="div">
+                    Song Name
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Artist Name
+                  </Typography>
+                </div>
+                <div className="playBtn">
+                  <i class="ss fa-solid fa-circle-play"></i>
+                </div>
+              </CardContent>
             </CardActionArea>
+            <i
+              class="slider-component2_heart fa-solid fa-plus float-end text-end"
+              onClick={() => setOpened(true)}
+            ></i>
+
+            <i class="slider-component2_heart fa-regular fa-heart float-end text-end"></i>
           </Card>
         </div>
 
         <div className="SliderComponent_2_card">
-          <Card className="cards-layout">
+          <Card className="cards-layout"
+          sx={{
+            backgroundColor: "#485461",
+            backgroundImage:
+              "linear-gradient(315deg, #485461 0%, #28313b 74%)",
+          }}>
             <CardActionArea>
               <CardMedia
                 component="img"
@@ -205,20 +315,26 @@ export default function SliderComponent_4() {
                 image={Login}
                 alt="green iguana"
               />
-              <CardContent className='cardComponent'>
-          <div className="card-heading">
-          <Typography gutterBottom variant="h5" component="div">
-            Song Name
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Artist Name
-          </Typography>
-          </div>
-          <div className="playBtn">
-          <i class="ss fa-solid fa-circle-play"></i>
-          </div>
-        </CardContent>
+              <CardContent className="cardComponent">
+                <div className="card-heading">
+                  <Typography gutterBottom variant="h5" component="div">
+                    Song Name
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Artist Name
+                  </Typography>
+                </div>
+                <div className="playBtn">
+                  <i class="ss fa-solid fa-circle-play"></i>
+                </div>
+              </CardContent>
             </CardActionArea>
+            <i
+              class="slider-component2_heart fa-solid fa-plus float-end text-end"
+              onClick={() => setOpened(true)}
+            ></i>
+
+            <i class="slider-component2_heart fa-regular fa-heart float-end text-end"></i>
           </Card>
         </div>
       </div>
